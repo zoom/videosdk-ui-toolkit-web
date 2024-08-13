f384b7c5aad249d676a092401f6947f07973c219
Quentin Hayot <quentinhayot@users.noreply.github.com>
2024-08-13 16:14:14 +0200

    Added missing components related method declarations
    index.d.tous was missing the components related methods. This commit fixes that and allows the use of components.

--- index.d.ts
+++ index.d.ts
@@ -7,5 +7,17 @@ declare namespace _default {
     function offSessionJoined(callback: EventListenerOrEventListenerObject): void;
     function onSessionClosed(callback: EventListenerOrEventListenerObject): void;
     function offSessionClosed(callback: EventListenerOrEventListenerObject): void;
+    function showUitoolkitComponents(container: HTMLElement, config: Object): void;
+    function hideUitoolkitComponents(container: HTMLElement): void;
+    function showVideoComponent(container: HTMLElement): void;
+    function hideVideoComponent(container: HTMLElement): void;
+    function showChatComponent(container: HTMLElement): void;
+    function hideChatComponent(container: HTMLElement): void;
+    function showControlsComponent(container: HTMLElement): void;
+    function hideControlsComponent(container: HTMLElement): void;
+    function showUserComponent(container: HTMLElement): void;
+    function hideUserComponent(container: HTMLElement): void;
+    function showSettingsComponent(container: HTMLElement): void;
+    function hideSettingsComponent(container: HTMLElement): void;
 }
 export default _default;
