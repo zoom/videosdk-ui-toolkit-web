454f3d8e87dfebef2d9bf1926ff1c06b8388b02f
Joyce Ma <43423318+PikaJoyce@users.noreply.github.com>
2025-07-15 10:59:32 -0700

    Update 2.2.5-1 (#81)
    * Update 2.2.5-1
    
    ---------
    
    Co-authored-by: Joyce Ma <joyce.ma@zoom.us>

--- index.d.ts
+++ index.d.ts
@@ -894,6 +894,12 @@ export interface UIToolkit {
    * @returns void
    */
   destroy(): void;
+
+  /**
+   * Get the version of the UI toolkit, Video SDK and Tailwind CSS
+   * @returns {Object} { uikit: string, videosdk: string, tailwindcss: string }
+   */
+  version(): { uikit: string; videosdk: string; tailwindcss: string };
 }
 
 declare const _default: UIToolkit;
